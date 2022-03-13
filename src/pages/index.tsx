56ab52ff812645ec717ad9e639ccfbcd77a074ef
hayashi-ay <go12limchangyong@gmail.com>
2022-03-13 20:15:08 +0900

    fix: compileエラーを修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Footer from '@organism/footer'
+import Footer from '@/organism/footer'
 
 const Home: NextPage = () => {
   return (
