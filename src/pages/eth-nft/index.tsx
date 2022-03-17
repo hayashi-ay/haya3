857e0f27bc9c3559ab5864a2f0bc969498b4d921
hayashi-ay <go12limchangyong@gmail.com>
2022-03-17 23:32:32 +0900

    fix: loading indicator

--- src/pages/eth-nft/index.tsx
+++ src/pages/eth-nft/index.tsx
@@ -48,7 +48,11 @@ const EthNFT = () => {
 					</button>
 				)}
 				{account && (
-					<button className="px-4 h-11 w-32 text-xl border-0 bg-gray-100 rounded-md" onClick={tryToMintNFT}>
+					<button className="px-4 inline-flex items-center h-11 w-36 text-xl border-0 bg-gray-100 rounded-md" onClick={tryToMintNFT}>
+						<svg className="animate-spin h-5 w-5 mr-2" viewBox="0 0 24 24">
+							<circle className="opacity-25" cx="12" cy="12" r="10" fill="none" stroke="currentColor" strokeWidth="4"></circle>
+							<path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+						</svg>
 						Mint NFT
 					</button>
 				)}
