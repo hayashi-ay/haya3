1a3836506e9802c65d4f1833d7727c129ca86ea7
hayashi-ay <go12limchangyong@gmail.com>
2022-03-18 18:46:13 +0900

    fix: MetaMaskが接続されていないときのエラーを修正

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -3,6 +3,8 @@ import { ethers } from "ethers";
 const useContract = (address: string, abi: any) => {
 	if (typeof window === "undefined")
 		return null
+	if (typeof window.ethereum === "undefined")
+		return null
 	const provider = new ethers.providers.Web3Provider(window.ethereum);
 	const signer = provider.getSigner();
 	const contract = new ethers.Contract(address, abi, signer);
