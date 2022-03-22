ff543d2f372d3b40703f743b6876c59cf30cad46
hayashi-ay <go12limchangyong@gmail.com>
2022-03-22 11:55:41 +0900

    chore: add alias for components

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   reactStrictMode: true,
   webpack(config, options) {
     config.resolve.alias['@organism'] = path.join(__dirname, 'src/components/organism/')
+    config.resolve.alias['@components'] = path.join(__dirname, 'src/components/')
     return config
   },
 }
