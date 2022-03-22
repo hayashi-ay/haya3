c9d78ccc25f41e7e87c2aff1d4b5246bcd8b7c7b
hayashi-ay <go12limchangyong@gmail.com>
2022-03-22 13:25:46 +0900

    feat: checkc metamask is connected and then show character select component

--- src/pages/nft-game/index.tsx
+++ src/pages/nft-game/index.tsx
@@ -1,13 +1,37 @@
 import Button from "@/components/atoms/button";
+import { useEffect } from "react";
+import useMetaMask from "src/hooks/useMetaMask";
 
 const NFTGame = () => {
+
+	const {
+		account,
+		connectMetaMask
+	} = useMetaMask()
+
+	useEffect(() => {
+		connectMetaMask();
+	}, [])
+
+	const SelectCharacter = () => {
+		return (
+			<div className="">
+				<h2>⏬ Mint your character. ⏬</h2>
+			</div>
+		);
+	};
 
 	return (
 		<div className="p-8 min-h-screen">
 			<div className="flex flex-col justify-center items-center">
-				<p className="text-center text-5xl font-semibold">NFT GAME</p>
-				<p className="my-4 text-center text-2xl">Defeat boss</p>
-				<Button btnTxt="Connect to wallet" onClick={() => console.log("hello")}></Button>
+				<p className="text-center text-5xl font-semibold">⚡️ METAVERSE GAME ⚡️</p>
+				<p className="my-4 text-center text-2xl">Team up to beat the Boss</p>
+				{!account && (
+					<Button btnTxt="Connect wallet to get started" onClick={connectMetaMask}></Button>
+				)}
+				{account && (
+					SelectCharacter()
+				)}
 			</div>
 		</div>
 	)
