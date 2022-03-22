77db1f0b0e2814c8b388b540f258b39d74d52a77
hayashi-ay <go12limchangyong@gmail.com>
2022-03-22 18:26:29 +0900

    feat: add hooks for MyEpicGame

---
+++ src/hooks/useMyEpicGameContract.ts
@@ -0,0 +1,62 @@
+import useContract from "./useContract"
+import myEpicGame from "../contracts/MyEpicGame.json"
+import { useEffect, useState } from "react"
+import { CharacterData } from "src/types/nft-game"
+import useMetaMask from "./useMetaMask"
+
+const toCharacterData = (data: any) => {
+	return {
+		name: data.name,
+		imageURI: data.imageURI,
+		hp: data.hp.toNumber(),
+		maxHp: data.maxHp.toNumber(),
+		attackDamage: data.attackDamage.toNumber()
+	}
+}
+
+const useMyEpicGameContract = () => {
+	const address = process.env.NEXT_PUBLIC_MY_EPIC_GAME_CONTRACT_ADDRESS as string
+	const contract = useContract(address, myEpicGame.abi)
+
+	const [characterNFT, setCharacterNFT] = useState<CharacterData | null>(null)
+	const [defaultCharacters, setDefaultCharacters] = useState<CharacterData[]>([])
+
+	const { account } = useMetaMask()
+
+	const fetchNFTMetadata = async () => {
+		const txn = await contract?.checkIfUserHasNFT()
+		console.log(txn)
+		if (txn.name) {
+			setCharacterNFT(toCharacterData(txn))
+		} else {
+			setCharacterNFT(null)
+		}
+	}
+
+	const getDefaultCharacters = async () => {
+		const txn = await contract?.getDefaultCharacters()
+		console.log(txn)
+		const characters = txn.map((data: any) => toCharacterData(data))
+		setDefaultCharacters(characters)
+	}
+
+	useEffect(() => {
+		fetchNFTMetadata()
+		getDefaultCharacters()
+	}, [account])
+
+	//const setListenerForNFTMinted = () => {
+	//	contract?.on("NewEpicNFTMinted", (from, tokenId) => {
+	//		console.log(from, tokenId)
+	//		setTokenId(tokenId)
+	//	})
+	//}
+
+	return {
+		characterNFT,
+		defaultCharacters,
+		address,
+	}
+}
+
+export default useMyEpicGameContract
