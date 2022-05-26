7d6a9b949a36785a568850983d5107bd4514a9c6
hayashi-ay <go12limchangyong@gmail.com>
2022-05-27 00:13:04 +0900

    fix: cssをブラッシュアップ

--- src/components/nft-game/Arena.tsx
+++ src/components/nft-game/Arena.tsx
@@ -9,23 +9,28 @@ const Arena = () => {
 	const attackToBoss = async () => { }
 
 	return (
-		<div>
+		<div className="text-center">
 			{boss && (
-				<div>
-					<h2>{boss.name}</h2>
-					<img className="w-64 h-64 z-10 object-cover" src={boss.imageURI} alt={boss.name}></img>
-					<div>
-						<progress value={boss.hp} max={boss.maxHp} />
-						<p>{`${boss.hp} / ${boss.maxHp} HP`}</p>
+				<div className="">
+					<div className="relative">
+						<h2>{boss.name}</h2>
+						<div>
+							<img className="w-64 h-64 object-cover" src={boss.imageURI} alt={boss.name}></img>
+							<div className="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700">
+								<div className="h-6 bg-blue-600 text-base font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: '45%' }}></div>
+								{`${boss.hp} / ${boss.maxHp} HP`}
+							</div>
+						</div>
 					</div>
 					<div>
-						<button onClick={attackToBoss} className="bottom-0 w-full h-10 text-xl font-bold bg-gray-200 rounded-b-lg">
+						<button onClick={attackToBoss} className="mt-12 w-full h-10 text-xl font-bold bg-gray-200 rounded-b-lg">
 							{`Attack ${boss.name}`}
 						</button>
 					</div>
 				</div>
-			)}
-		</div>
+			)
+			}
+		</div >
 	)
 }
 export default Arena;
