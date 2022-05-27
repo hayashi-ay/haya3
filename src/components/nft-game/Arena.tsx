dfa4d9bb9bc312e36c5941dd86ed7201be3a61a2
hayashi-ay <go12limchangyong@gmail.com>
2022-05-27 16:05:47 +0900

    fix: bossのcssを調整

--- src/components/nft-game/Arena.tsx
+++ src/components/nft-game/Arena.tsx
@@ -14,10 +14,10 @@ const Arena = () => {
 		<div className="text-center">
 			{boss && (
 				<div className="">
-					<div className="relative">
+					<div className="relative p-4 rounded-lg bg-gray-200">
 						<h2 className="font-bold text-3xl">{boss.name}</h2>
 						<div>
-							<img className="w-64 h-40 object-cover" src={boss.imageURI} alt={boss.name}></img>
+							<img className="w-64 h-40 object-cover mb-2" src={boss.imageURI} alt={boss.name}></img>
 							<div className="relative w-full h-6 bg-red-400 rounded-full dark:bg-gray-700">
 								<div className="absolute h-6 bg-green-400 text-base font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${boss.hp / boss.maxHp * 100}%` }}></div>
 								<div className="absolute w-full font-bold">{`${boss.hp} / ${boss.maxHp} HP`}</div>
@@ -25,7 +25,7 @@ const Arena = () => {
 						</div>
 					</div>
 					<div>
-						<button onClick={attackToBoss} className="mt-4 w-9/12 h-10 text-xl font-bold bg-gray-200 rounded-lg">
+						<button onClick={attackToBoss} className="mt-4 w-9/12 h-10 text-xl font-bold bg-red-200 rounded-lg">
 							{`Attack ${boss.name}`}
 						</button>
 					</div>
