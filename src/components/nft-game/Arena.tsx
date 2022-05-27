d5162b1da22a4a40aebf855c28b940b6083ea9d3
hayashi-ay <go12limchangyong@gmail.com>
2022-05-27 16:50:59 +0900

    feat: characterの描画

--- src/components/nft-game/Arena.tsx
+++ src/components/nft-game/Arena.tsx
@@ -30,7 +30,7 @@ const Arena = () => {
 						<div>
 							<img className="w-64 h-40 object-cover mb-2" src={boss.imageURI} alt={boss.name}></img>
 							<div className="relative w-full h-6 bg-red-400 rounded-full dark:bg-gray-700">
-								<div className="absolute h-6 bg-green-400 text-base font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${boss.hp / boss.maxHp * 100}%` }}></div>
+								<div className="absolute h-6 bg-green-400 leading-none rounded-full" style={{ width: `${boss.hp / boss.maxHp * 100}%` }}></div>
 								<div className="absolute w-full font-bold">{`${boss.hp} / ${boss.maxHp} HP`}</div>
 							</div>
 						</div>
@@ -41,25 +41,28 @@ const Arena = () => {
 						</button>
 					</div>
 				</div>
-			)}
-			{characterNFT && (
-				<div className="mt-12">
-					<h2 className="font-bold text-2xl">Your Character</h2>
-					<div className="relative mt-4 p-4 rounded-lg bg-gray-200">
-						<h2 className="font-bold text-xl">{characterNFT.name}</h2>
-						<div>
-							<img className="w-40 h-40 object-cover mb-2" src={characterNFT.imageURI} alt={characterNFT.name}></img>
-							<div className="relative w-full h-6 bg-red-400 rounded-full dark:bg-gray-700">
-								<div className="absolute h-6 bg-green-400 text-base font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${characterNFT.hp / characterNFT.maxHp * 100}%` }}></div>
-								<div className="absolute w-full font-bold">{`${characterNFT.hp} / ${characterNFT.maxHp} HP`}</div>
-							</div>
-							<div className="mt-4">
-								<h4>{`⚔️ Attack Damage: ${characterNFT.attackDamage}`}</h4>
+			)
+			}
+			{
+				characterNFT && (
+					<div className="mt-12">
+						<h2 className="font-bold text-2xl">Your Character</h2>
+						<div className="relative mt-4 p-4 rounded-lg bg-gray-200">
+							<h2 className="font-bold text-xl">{characterNFT.name}</h2>
+							<div>
+								<img className="w-40 h-40 object-cover mb-2" src={characterNFT.imageURI} alt={characterNFT.name}></img>
+								<div className="relative w-full h-6 bg-red-400 rounded-full dark:bg-gray-700">
+									<div className="absolute h-6 bg-green-400 leading-none rounded-full" style={{ width: `${characterNFT.hp / characterNFT.maxHp * 100}%` }}></div>
+									<div className="absolute w-full font-bold">{`${characterNFT.hp} / ${characterNFT.maxHp} HP`}</div>
+								</div>
+								<div className="mt-4">
+									<h4>{`⚔️ Attack Damage: ${characterNFT.attackDamage}`}</h4>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			)}
+				)
+			}
 		</div >
 	)
 }
