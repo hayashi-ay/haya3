a515dd9d4454b103226463ef29b344e9d9bbb08d
hayashi-ay <go12limchangyong@gmail.com>
2022-05-27 17:09:16 +0900

    feat: loading buttonをコンポーネント化

--- src/pages/eth-nft/index.tsx
+++ src/pages/eth-nft/index.tsx
@@ -1,3 +1,4 @@
+import Loading from "@/components/atoms/loading";
 import React, { useEffect, useState } from "react";
 import useMetaMask from "src/hooks/useMetaMask";
 import useMyEpicNFTContract from "src/hooks/useMyEpicNFTContract";
@@ -56,12 +57,7 @@ const EthNFT = () => {
 				)}
 				{account && !tokenId && (
 					<button disabled={isMinting} className="px-4 inline-flex items-center justify-center h-11 w-36 text-xl border-0 bg-gray-100 rounded-md" onClick={tryToMintNFT}>
-						{isMinting && (
-							<svg className="animate-spin h-5 w-5 mr-2" viewBox="0 0 24 24">
-								<circle className="opacity-25" cx="12" cy="12" r="10" fill="none" stroke="currentColor" strokeWidth="4"></circle>
-								<path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-							</svg>
-						)}
+						{isMinting && (<Loading></Loading>)}
 						{isMinting ? "Minting..." : "Mint NFT"}
 					</button>
 				)}
