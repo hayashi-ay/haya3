33a65a788e48a7a53998405e8e36908290aeaf7d
Ayato Hayashi <ayato.hayashi@justincase-tech.com>
2023-02-02 18:00:25 +0900

    haya33

--- src/components/organism/header.tsx
+++ src/components/organism/header.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const Header = () => {
   return (
     <Head>
-      <title>haya33</title>
+      <title>haya3</title>
       <meta name="description" content="haya3 gallery" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
